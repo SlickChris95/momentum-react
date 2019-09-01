20f629a2232b995b2b89891be34aeb06dd586f5f
SlickChris95 <torres.christian33@yahoo.com>
2019-09-01 14:53:57 -0700

    added openWeatherAppAPI

--- src/components/App.js
+++ src/components/App.js
@@ -1,28 +1,34 @@
 import React from 'react';
 import Time from './Time.js';
 import Quote from './Quote.js';
-import Weather from '../api/Weather.js';
+// import Weather from '../api/Weather.js';
+import axios from 'axios'
+
 
 class App extends React.Component{
 
-weatherClick = async()=>{
-  const response = await Weather.get('/data/2.5/weather?',{
-      params: {zip: '94040'},
-  });
-  console.log(this)
-}
+  weatherClick = ()=>{
+    // axios.get('api.openweathermap.org/data/2.5/weather?zip=92880&APPID=815d38de5e85a1a57a6bb43292ea5844')
+    axios
+      .get('http://api.openweathermap.org/data/2.5/weather?zip=92880,us&appid=815d38de5e85a1a57a6bb43292ea5844')
+      .then((response)=>{
+        console.log(response.data.main.temp)
+        console.log('hi')
+      });
 
+  }
 
-render(){
-  return(
-    <div id="app-center-container">
-      <Time />
-      <Quote />
-      <button onClick= {this.weatherClick}>Test</button>
-    </div>
-  );
-}
+// http://api.openweathermap.org/data/2.5/weather?zip='+ zip +',us&appid=815d38de5e85a1a57a6bb43292ea5844'
 
+  render(){
+    return(
+      <div id="app-center-container">
+        <Time />
+        <Quote />
+        <button onClick= {this.weatherClick}>Test</button>
+      </div>
+    );
+  }
 }
 
 export default App
